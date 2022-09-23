1da78bee60eb9432accbbae05dafa644ba95dd99
medicalanthrax <109490810+medicalanthrax@users.noreply.github.com>
2022-09-22 22:55:57 -0500

    color my views

---
+++ Vibrator/app/src/main/java/com/example/vibrator/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.vibrator
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Vibrator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
